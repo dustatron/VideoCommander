437fe9aa9df60642e126a6acea5e67ab8303d91f
Micah Geisel <micah@botandrose.com>
2017-04-07 15:13:23 -0700

    refactor to model and view architecture.

--- public/script.js
+++ public/script.js
@@ -1,178 +1,113 @@
-////// google Firebase Initialize /////
-var config = {
-  apiKey: "AIzaSyDxdKOEazxOtcyJNqo4T4RpVzRvHEnh62E",
-  authDomain: "video-commander.firebaseapp.com",
-  databaseURL: "https://video-commander.firebaseio.com",
-  projectId: "video-commander",
-  storageBucket: "video-commander.appspot.com",
-  messagingSenderId: "643393373733"
+const CommentsView = function(comments) {
+  let render = function() {
+    let $comments = document.getElementById("comment-list");
+
+    //---- clear ----//
+    $comments.innerHTML = "";
+
+    //----- write ----//
+    comments.forEach(function(comment) {
+      $comments.innerHTML +=
+        "<div class='comment-element text-left'>"
+        + '<span class="comment-name">' + comment.name + '</span> : '
+        + '<span class="marker-time">' + comment.markerAt + '</span> : '
+        + '<span class="date-of-commnet">' + comment.date + '</span> <br/>'
+        + '<span class="text-of-commnet">' + comment.comment + '</span>';
+    });
+  };
+
+  comments.onUpdate(function() {
+    render();
+  });
 };
-firebase.initializeApp(config);
 
-///////// Global Variables  ///////
+const NewCommentView = function(comments) {
+  let $submitButton = document.getElementById("submit");
 
-var video = document.getElementById("video1");
+  $submitButton.addEventListener("click", function() {
+    let $video = document.getElementById("video1");
+    let today = new Date();
+    let date = today.getDate() + ' / ' + (today.getMonth()+1) + ' / ' + today.getFullYear();
+    let name = document.getElementById("name").value;
+    let markerAt = formatSeconds($video.currentTime);
+    let second = $video.currentTime;
+    let commentText = document.getElementById("add-comment").value;
 
-///// Reference to Firebase Database
+    let comment = {
+      date: date,
+      name: name,
+      markerAt: markerAt,
+      second: second,
+      comment: commentText,
+    };
 
-const dbRef = firebase.database().ref().child('comments');
+    comments.push(comment);
+  });
 
+  function formatSeconds(seconds) {
+    let min = Math.floor(seconds / 60);
+    let sec = Math.floor(seconds % 60);
 
-//--------------- VIDEO CONTROS ---------------------////
-
-//------- SHUTTLE VARIABLES  ----//
-var scrubSlider = document.getElementById("scrubSlider");
-var currentTimeDisplay = document.getElementById("currentTimeField");
-
-// --- on click fucntions ---//
-document.getElementById("play").addEventListener("click", shuttlPlay);
-
-//---- Mouse Events ---///
-video.addEventListener("timeupdate", movePlaySlider);
-video.addEventListener("timeupdate", getCurrentTime);
-scrubSlider.addEventListener("mousedown", pauseSlider); // drag slider
-scrubSlider.addEventListener("mouseup", resumeSlider); // resume slider
-scrubSlider.addEventListener("change", scrubVideo); // check for video scrub value changes
-
-// ----- submit button listenr -----//
-document.getElementById("submit").addEventListener("click",addComment)
-
-/*
-////////// functions /////////////
-*/
-
-//// passing dbRef as object to printToDoM() ///
-dbRef.on("value", function(snapshot) {
-  printToDom(snapshot.val());
-}, function (errorObject) {
-  console.log("The read failed: " + errorObject.code);
-});
-
-
-//----checks if pause player is checked. if it is it pause the player.
-function pausePlayer(){
-  var checkBox = document.getElementById("play-back").checked;
-  if (checkBox){
-    return video.pause();
-  }
-}//end pausePlayer
-
-
-function scrubVideo() {
-  var scrubTime = video.duration * (scrubSlider.value / 100);
-  video.currentTime = scrubTime; // sets the current play time to the value of the slide bar
-}
-
-function movePlaySlider() {
-  var playBackPoint = (100 / video.duration) * video.currentTime;
-  scrubSlider.value = playBackPoint; //sets the paybar value to the current palyback time value
-}
-
-function pauseSlider() {
-  video.pause(); // pauses the video when the slider his pressed
-}
-
-function resumeSlider() //
-{
-  video.play(); // resumes the slider when the video is pressed
-  pausePlayer();
-}
-
-
-// gets playback time
-function getCurrentTime(){
-  var currentTime = video.currentTime;
-  var minutes = Math.floor(currentTime / 60);
-  var seconds = Math.floor(currentTime % 60);
-
-  if (minutes < 10){
-    minutes = "0" + minutes;
-  } else if (seconds < 10){
-    seconds = "0" + seconds;
-  }
-  currentTimeDisplay.setAttribute("value", (minutes + ":" + seconds));
-}
-
-
-///// ads user comment to db ////
-function addComment(){
-  ///// human readable time ///
-  var today = new Date();
-
-  var date = today.getDate()+' / '
-    +(today.getMonth()+1)+' / '
-    +today.getFullYear();
-
-  //// create new object from comment ////
-  var newSubmit = new commentCreator(date);
-
-  /// push comment to db ////
-  dbRef.push(newSubmit);
-  document.getElementById('add-comment').value = "";
-}//end adComment
-
-function videoTimeReadOut (){
-  var currentFrame = video.currentTime;
-  var min = Math.floor(currentFrame / 60);
-  var sec = Math.floor(currentFrame % 60);
-
-  if (min < 10){
-    min = "0" + min;
-  }
-  if (sec < 10) {
-    sec = "0" + sec;
-  }
-  var readOut = min + ":" + sec;
-
-  return readOut; //display the video durration
-}
-
-
-// -- creates new object with parameters.
-function commentCreator(date) {
-  this.date = date;
-  this.name = document.getElementById("name").value;
-  this.markerAt = videoTimeReadOut();
-  this.second = video.currentTime;
-  this.comment =  document.getElementById("add-comment").value;
-}
-
-/// clears list then re-writes comment list ///
-function printToDom (e){
-  var printMe = Object.values(e);
-  let commentMenu = document.getElementById("comment-list");
-
-  //---- clear ----//
-  commentMenu.innerHTML = "";
-
-  //----- write ----//
-  for (var i = 0; i < printMe.length; i++){
-    commentMenu.innerHTML +=
-      "<div class='comment-element text-left' onclick='goTo("
-      + printMe[i].second +")'>"
-      + printMe[i].name + ' : '
-      + '<span class="marker-time">'+printMe[i].markerAt + '</span> : '
-      + '<span class="date-of-commnet">' + printMe[i].date + '</span> <br/>'
-      + '<span class="text-of-commnet">'+printMe[i].comment + '</span>';
-  }// End For Loop
-}// End print to DOM
-
-// ---- shuttle functions --- //
-function shuttlPlay(){
-  if(video.paused){
-    video.play();
-  } else {
-    video.pause();
+    if (min < 10){
+      min = "0" + min;
+    }
+    if (sec < 10) {
+      sec = "0" + sec;
+    }
+    return min + ":" + sec;
   }
 }
 
-function shuttlPause(){
-  video.pause();
+const Comments = function() {
+  this.forEach = function(callback) {
+    comments.forEach(callback);
+  };
+
+  this.onUpdate = function(callback) {
+    updateCallbacks.push(callback);
+  }
+
+  this.push = function(comment) {
+    commentsSubscription.push(comment);
+  }
+
+  // private
+
+  let comments = [];
+  let updateCallbacks = [];
+  let commentsSubscription = null;
+  const that = this;
+
+  wireUpFirebase = function() {
+    const config = {
+      apiKey: "AIzaSyDxdKOEazxOtcyJNqo4T4RpVzRvHEnh62E",
+      authDomain: "video-commander.firebaseapp.com",
+      databaseURL: "https://video-commander.firebaseio.com",
+      projectId: "video-commander",
+      storageBucket: "video-commander.appspot.com",
+      messagingSenderId: "643393373733"
+    };
+    firebase.initializeApp(config);
+
+    commentsSubscription = firebase.database().ref().child('comments');
+    commentsSubscription.on("value", function(snapshot) {
+      comments = Object.values(snapshot.val());
+      updateCallbacks.forEach(function(callback) {
+        callback();
+      });
+    }, function (errorObject) {
+      console.log("The read failed: " + errorObject.code);
+    });
+  }
+
+  wireUpFirebase();
+};
+
+function boot() {
+  const comments = new Comments();
+  const commentsView = new CommentsView(comments);
+  const newCommentView = new NewCommentView(comments);
 }
 
-//---- allows comments to jump to time in movie---//
-function goTo(e){
-  video.currentTime = e;
-  pausePlayer()
-}
+boot();
 
